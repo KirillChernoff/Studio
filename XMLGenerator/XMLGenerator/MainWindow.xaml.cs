2d5ec56c8285c6ebd71e757e37c5a8aec84c8315
RannyRanny <mr.lolnub@gmail.com>
2015-07-31 14:14:14 +0600

    Initialize

---
+++ XMLGenerator/XMLGenerator/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XMLGenerator
+{
+
+    public partial class MainWindow : Window
+    {
+        public void ExitClick(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void ChooseNewButtonClick(object sender, System.EventArgs e)
+        {
+            tabControl1.SelectedIndex = 1;
+        }
+
+        public void ChooseEditButtonClick(object sender, System.EventArgs e)
+        {
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Template3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Template4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Template1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Template2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Template5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Template6Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Template8Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Template7Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XMLGenerator/XMLGenerator/New.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLGenerator
+{
+   
+    public class Table
+    {
+        public int GetCoordRow(string Coord)
+        {
+            char[] splitters = { ' ' };
+            int row;
+            string[] temp= Coord.Split(splitters);
+            row = Convert.ToInt32(temp[0], 10);
+            return row;
+        }
+
+        public int GetCoordCol(string Coord)
+        {
+            char[] splitters = { ' ' };
+            int col;
+            string[] temp = Coord.Split(splitters);
+            col = Convert.ToInt32(temp[1], 10);
+            return col;
+        }
+
+        public string SetCoord(int row, int col)
+        {
+            string coord = row.ToString() + ' ' + col.ToString();
+            return coord;
+        }
+
+        public string GetCell(int row,int col, Dictionary<string,string> dict)
+        {
+            string val;
+            val = dict[SetCoord(row, col)];
+            return val;
+        }
+        
+        public Table FillTable()
+        {
+            Table table=new Table();
+
+            return table;
+        }
+
+        public void SetCell(int row, int col, string val, Dictionary<string, string> dict)
+        {
+            dict.Add(SetCoord(row, col), val);
+        }
+
+        int _rowNum
+        {
+            get
+            {
+                return _rowNum;
+            }
+            set
+            {
+                _rowNum = value;
+            }
+        }
+        int _colNum
+        {
+            get
+            {
+                return _colNum;
+            }
+            set
+            {
+                _colNum = value;
+            }
+        }
+        Dictionary<string, string> _cells;
+        public Table()
+        {
+            _colNum = 0;
+            _rowNum = 0;
+            _cells = new Dictionary<string, string>();
+        }
+        public Table(int rowNum, int colNum, Dictionary<string,string> cells)
+        {
+            this._colNum = colNum;
+            this._rowNum = rowNum;
+            this._cells = cells;
+        }
+    }
+    static class  New
+    {
+    }
+}
