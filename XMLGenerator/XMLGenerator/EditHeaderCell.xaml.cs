a98e396c5a5405f2eea58f17d638ed6e5915bb17
RannyRanny <mr.lolnub@gmail.com>
2015-08-07 16:13:45 +0500

    added EditHeaderCell.Xaml.CS file

---
+++ XMLGenerator/XMLGenerator/EditHeaderCell.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XMLGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для EditHeaderCell.xaml
+    /// </summary>
+    public partial class EditHeaderCell : Window
+    {
+        
+        
+        public EditHeaderCell()
+        {
+            
+            InitializeComponent();
+        }
+        
+        internal  void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+            Logic.SaveHeader(this,MainWindow.GetObjectXML());
+            }
+            catch (FormatException)
+            {
+                Logic.ErrorDialog();
+                return;
+            }
+            
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
